a6499a42f640a5f7a97a63f07819f9472ab3af3c
Br0cod3 <thespammingbott@gmail.com>
2025-09-28 16:59:54 +0100

    implement: fetchWeatherData and getWeatherData functions

---
+++ src/weather.js
@@ -0,0 +1,21 @@
+
+async function fetchWeatherData(location) {
+    const key = "3VHSNTX2HAD5JRCZCWJL42ZWJ"
+    const searchURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`
+    const response = await fetch(searchURL)
+    const data = await response.json()
+    return getWeatherData(data)
+}
+
+function getWeatherData(data) {
+    const latitude = data.latitude
+    const longitude = data.longitude
+    const condition = data.days[0].conditions
+    const description = data.days[0].description
+    const pressure = data.days[0].pressure
+    const humidity = data.days[0].humidity
+    const visibility = data.days[0].visibility
+    return { latitude, longitude, condition, description, pressure, humidity, visibility }
+}
+
+fetchWeatherData("london").then(data => console.log(data))
