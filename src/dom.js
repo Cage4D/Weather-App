79637493c419b1bd69391d9f3265f8f13bc36538
Br0cod3 <thespammingbott@gmail.com>
2025-09-29 17:52:50 +0100

    implement: domElements

---
+++ src/dom.js
@@ -0,0 +1,16 @@
+
+const domElements = (function() {
+    const  input = document.querySelector("#searchBar")
+    const location = document.querySelector(".info__location-name")
+    const temperature = document.querySelector(".info__temperature")
+    const condition = document.querySelector(".info__condition")
+    const description = document.querySelector(".info__description")
+    const latitude = document.querySelector("#lat")
+    const longitude = document.querySelector("#long")
+    const pressure = document.querySelector("#prez")
+    const humidity = document.querySelector("#humid")
+
+    return { input, location, temperature, condition, description, latitude, longitude, pressure, humidity }
+})()
+
+
