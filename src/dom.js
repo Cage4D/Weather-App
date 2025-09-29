e0a7a2803c569cc45d9c4b9e72e9c9be34a6e675
Br0cod3 <thespammingbott@gmail.com>
2025-09-29 21:59:08 +0100

    add: pText, hText, lonText and latText, also formatted with prettier

--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,33 @@
+const domElements = (function () {
+  const input = document.querySelector("#searchBar");
+  const location = document.querySelector(".info__location-name");
+  const temperature = document.querySelector(".info__temperature");
+  const condition = document.querySelector(".info__condition");
+  const description = document.querySelector(".info__description");
+  const latitude = document.querySelector("#lat");
+  const longitude = document.querySelector("#long");
+  const pressure = document.querySelector("#prez");
+  const humidity = document.querySelector("#humid");
+  const pText = document.querySelector(".prop__latitude");
+  const hText = document.querySelector(".prop__longitude");
+  const latText = document.querySelector(".prop__pressure");
+  const lonText = document.querySelector(".prop__humidity");
 
-const domElements = (function() {
-    const  input = document.querySelector("#searchBar")
-    const location = document.querySelector(".info__location-name")
-    const temperature = document.querySelector(".info__temperature")
-    const condition = document.querySelector(".info__condition")
-    const description = document.querySelector(".info__description")
-    const latitude = document.querySelector("#lat")
-    const longitude = document.querySelector("#long")
-    const pressure = document.querySelector("#prez")
-    const humidity = document.querySelector("#humid")
+  return {
+    input,
+    location,
+    temperature,
+    condition,
+    description,
+    latitude,
+    longitude,
+    pressure,
+    humidity,
+    pText,
+    hText,
+    latText,
+    lonText,
+  };
+})();
 
-    return { input, location, temperature, condition, description, latitude, longitude, pressure, humidity }
-})()
-
-export default domElements
+export default domElements;
