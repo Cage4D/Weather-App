e2d246dc03b4add8b0f0eae341dabe873361716d
Br0cod3 <thespammingbott@gmail.com>
2025-09-30 06:41:29 +0100

    import: cloudy, sunny and rain images

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ import "../styles/components/prop.css"
 import "../styles/utils.css"
 import weatherData from "./weather.js"
 import Dom from "./dom.js"
+import cloudy from "../img/cloudy.jpg";
+import sunny from "../img/sunny.jpg";
+import rain from "../img/rain.jpg";
